58eb3b4e95ab1d85a3ab74977a90c32bd9ffb38d
joeymu <jomu7038@colorado.edu>
2025-07-29 01:04:33 +0000

    update version of psycopg

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-import psycopg2
+import psycopg
 
 app = Flask(__name__)
 
@@ -10,6 +10,6 @@ def hello_world():
 @app.route('/db_test')
 def test_db():
     db_url = 'postgresql://jm_db_2c3z_user:byXbToPGSweTUlwrlbE9BL5zS4U12AuM@dpg-d241lgfdiees73a91fjg-a/jm_db_2c3z'
-    conn = psycopg2.connect(db_url)
+    conn = psycopg.connect(db_url)
     conn.close()
     return 'Database Connection Successful'
